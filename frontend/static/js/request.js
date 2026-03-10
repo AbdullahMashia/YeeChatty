0d75f06e7c47f51e09d8c3f75cd5c81d87453d5b
aw <mad8zad@gmail.com>
2026-03-10 22:11:53 +0300

    added outgoing rquests

--- frontend/static/js/request.js
+++ frontend/static/js/request.js
@@ -1,4 +1,5 @@
 
+
 let requests = document.getElementById("requests");
 let request_square = document.getElementById("request_square");
 
@@ -12,32 +13,53 @@ let in_users = ["ali", "abdullah", "Saleh", "mohammed","ahmed",
 let i_r = document.getElementById("in");
 let o_r = document.getElementById("out");
 
+let m  = Math.random() * 15;
+let x = Math.random() * 10;
 
 
-
-
-
-    for(let i=0; i<15;i++)
-    {
-        let req = document.createElement("li");
+for( let a =1; a<4; a++)
+{
+ let req = document.createElement("li");
         let accept_request = document.createElement("h3");
         let refuse_request = document.createElement("h3");
+         let user = document.createElement("p");
+        accept_request.innerText="✅";
+        refuse_request.innerText ="❌";
+         if(a%2==0)
+         {
         let ls = document.createElement("ul");
         ls.id="check";
         ls.appendChild(accept_request);
         ls.appendChild(refuse_request);
-        let user = document.createElement("p");
+
 
         req.appendChild(user);
         req.appendChild(ls);
 
-        user.innerText = in_users[i];
-        accept_request.innerText="✅";
-        refuse_request.innerText ="❌";
+        user.innerText = in_users[a];
+
 
         req.classList.add("in_req")
         requests.appendChild(req);
+
+         }
+
+
+
+    else{
+
+
+        user.innerText = in_users[a];
+        req.appendChild(user);
+        req.appendChild(refuse_request);
+        req.classList.add("o_req");
+        requests.appendChild(req);
+
     }
+
+
+}
+
 
 
 request_square.scrollTop =0;
