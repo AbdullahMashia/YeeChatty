4e5cab329b7794d2e5c6c89d41e218d67c14f19e
Abdullah Mashia <999mashia@gmail.com>
2026-03-29 02:06:41 +0300

    Update my_sock.py

--- backend/my_sock.py
+++ backend/my_sock.py
@@ -9,52 +9,51 @@ from db import MyDataB
 
 import random
 
-user_data = dict()
+
 
 
 
 db_ob = MyDataB()
 
-room = 0
+class sock:
 
 
-@my_sock_ob.on('join_room')
-def joining_room(data):
-
-
-
-    room = f"room:{data["conv_id"]}"
-
-    user_id_soc = data.get("user_soc_id")
-
-    join_room(room)
-    emit('join_room', {"success":True,"m":"joined into room successfully","room":room})
-
-
-@my_sock_ob.on('send_message')
-def sending_message(data):
-
-
-
-
-
-    res = db_ob.send_message(data)
-
-    emit('new_message',data,room=room)
-
-
-@my_sock_ob.on('connect')
-def handle_connect():
-    user_data["id"] = session["user_id"]
-    user_data["username"] = session["username"]
-    user_data["cur_room_id"]= session["conv_id"]
-
-
-
-
-@my_sock_ob.on('disconnect')
-def disc(d):
-    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>disconnected")
+    
+    @my_sock_ob.on('join_room')
+    def joining_room(self,data):
+    
+    
+    
+        
+    
+     
+    
+        join_room( session["cur_room"])
+        emit('join_room', {"success":True,"m":"joined into room successfully","room": session["cur_room"]})
+    
+    
+    @my_sock_ob.on('send_message')
+    def sending_message(self,data):
+    
+    
+    
+    
+    
+        res = db_ob.send_message(data)
+    
+        emit('new_message',data,room= session["cur_room"])
+    
+    
+    @my_sock_ob.on('connect')
+    def handle_connect(self):
+        session["cur_room"]= session["conv_id"]
+    
+    
+    
+    
+    @my_sock_ob.on('disconnect')
+    def disc(self):
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>disconnected")
 
 
 
